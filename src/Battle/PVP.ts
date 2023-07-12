6bb1c04b86e6bf0902b3cb18399176ff12d3bb1a
Lucas Matos <lucaslmatos18@gmail.com>
2023-07-11 23:09:21 -0300

    REQ 10 - PVP Class

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,25 @@
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+class PVP extends Battle {
+  private _playerTwo: Fighter;
+  
+  constructor(player:Fighter, playerTwo: Fighter) {
+    super(player);
+    this._playerTwo = playerTwo;
+  }
+
+  fight(): number {
+    this.player.attack(this._playerTwo);
+    if (this._playerTwo.lifePoints === -1) { 
+      return 1; 
+    }
+    this._playerTwo.attack(this.player);
+    if (this.player.lifePoints === -1) { 
+      return -1; 
+    } 
+    return this.fight();
+  }
+}
+
+export default PVP;
