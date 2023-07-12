a86d19d83f196f275bfd978a77ec16986ab64361
Lucas Matos <lucaslmatos18@gmail.com>
2023-07-12 13:24:33 -0300

    REQ 11 - PVE Class (Agora passou)

--- src/Battle/PVE.ts
+++ src/Battle/PVE.ts
@@ -2,9 +2,9 @@ import Fighter, { SimpleFighter } from '../Fighter';
 import Battle from './Battle';
 
 class PVE extends Battle {
-  private _opponents: Array<SimpleFighter | Fighter>;
+  private _opponents: Fighter[] | SimpleFighter[];
   
-  constructor(player:Fighter, oponents: Array<SimpleFighter | Fighter>) {
+  constructor(player:Fighter, oponents: Fighter[] | SimpleFighter[]) {
     super(player);
     this._opponents = oponents;
   }
@@ -13,18 +13,16 @@ class PVE extends Battle {
     this.player.attack(this._opponents[0]);
     if (this._opponents[0].lifePoints === -1) {
       this._opponents.splice(0, 1);
-      if (this._opponents.length === 0) {
-        return 1;
-      }
     }
-
-    this._opponents.forEach((opponent) => {
-      opponent.attack(this.player);
+    if (this._opponents.length === 0) {
+      return 1;
+    }
+    for (let i = 0; i < this._opponents.length; i += 1) {
+      this._opponents[i].attack(this.player);
       if (this.player.lifePoints === -1) {
         return -1;
       }
-    });
-
+    }
     return this.continueFight();
   }
 
--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -12,4 +12,5 @@ interface Fighter {
   receiveDamage(attackPoints:number):number;
 }
 
+export { SimpleFighter };
 export default Fighter;
