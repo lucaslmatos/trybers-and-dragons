606fb50fe9955243d4533f8d46f9b0be27241895
Lucas Matos <lucaslmatos18@gmail.com>
2023-07-11 22:16:24 -0300

    REQ 09 - Monster Class

---
+++ src/Monster.ts
@@ -0,0 +1,29 @@
+import { SimpleFighter } from './Fighter';
+
+class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+
+  constructor() {
+    this._lifePoints = 85;
+    this._strength = 63;
+  }
+
+  get lifePoints():number { return this._lifePoints; }
+  get strength():number { return this._strength; }
+
+  receiveDamage(attackPoints:number):number {
+    if (attackPoints >= this._lifePoints) {
+      this._lifePoints = -1;
+    } else {
+      this._lifePoints -= attackPoints;
+    }
+    return this._lifePoints;
+  }
+
+  attack(enemy:SimpleFighter):void {
+    enemy.receiveDamage(this._strength);
+  }
+}
+
+export default Monster;
